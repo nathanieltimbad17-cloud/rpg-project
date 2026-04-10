f3a04d4d1cb215f90cef77571f964fe4bd009463
nathanieltimbad17-cloud <nathanieltimbad17@gmail.com>
2026-04-11 00:25:13 +0800

    Update LocalHealth.lua

--- LocalHealth.lua
+++ LocalHealth.lua
@@ -1,4 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 
 local Stats = require(ReplicatedStorage.Modules.Stats)
 
@@ -9,22 +10,39 @@ local stats = player:WaitForChild("Stats")
 local healthRegeneration = stats:WaitForChild("HealthRegeneration")
 
 local headUI = playerGui:WaitForChild("HeadUI")
-
 local healthFrame : Frame = headUI.HealthFrame
-local barFrame = healthFrame.BarFrame
-local healthTextLabel = healthFrame.HealthTextLabel
-local healthRegenerationTextLabel = healthFrame.RegenerationTextLabel
+
+local barFrame : Frame = healthFrame.BarFrame
+barFrame.ZIndex = 2
+
+local healthTextLabel : TextLabel = healthFrame.HealthTextLabel
+healthTextLabel.Transparency = 1
+healthTextLabel.ZIndex = 3
+
+local healthRegenerationTextLabel : TextLabel = healthFrame.RegenerationTextLabel
+healthRegenerationTextLabel.Transparency = 1
+healthRegenerationTextLabel.ZIndex = 3
+
+local damageFrame : Frame = healthFrame.DamageFrame
+damageFrame.ZIndex = 1
+
+local damageSpeed = healthFrame.DamageSpeed
+
+local function Round(number, decimals)
+    local power = math.pow(10, decimals)
+    return math.round(number * multiplier) / power
+end
 
 local function UpdateHealthRegeneration()
-    local healthRegenerationText = "+"..healthRegeneration.Value
+    local healthRegenerationText = "+"..Round(healthRegeneration.Value, 2)
     healthRegenerationTextLabel.Text = healthRegenerationText
 end
 
 local function OnCharacterAdded(character)
     local humanoid : Humanoid = character:WaitForChild("Humanoid")
     
-    local function UpdateBarFrame()
-        local healthPercent = Stats.GetHealtPercent(humanoid)
+    local function UpdateHealthBar()
+        local healthPercent = Stats.GetHealthPercent(humanoid)
         
         barFrame.Size = UDim2.new(healthPercent, 0, 1, 0)
         
@@ -32,17 +50,30 @@ local function OnCharacterAdded(character)
         healthTextLabel.Text = healthText
     end
     
-    local healthChanged = humanoid.HealthChanged:Connect(UpdateBarFrame)
+    local function UpdateHealthDamage(deltaTime)
+        local damageScale = math.max(damageFrame.Size.X.Scale - deltaTime * damageSpeed.Value, Stats.GetHealthPercent(humanoid))
+        
+        damageFrame.Size = UDim2.new(damageScale, 0, 1, 0)
+    end
+    
+    local healthChanged = humanoid.HealthChanged:Connect(UpdateHealthBar)
     local healthRegenerationChanged = healthRegeneration.Changed:Connect(UpdateHealthRegeneration)
+    local heartbeat = RunService.Heartbeat:Connect(UpdateHealthDamage)
     
     humanoid.Died:Once(function()
         healthChanged:Disconnect()
         healthRegenerationChanged:Disconnect()
+        heartbeat:Disconnect()
     end)
     
     UpdateBarFrame()
     UpdateHealthRegeneration()
+    UpdateHealthDamage()
     
+end
+
+if player.Character then
+    OnCharacterAdded(player.Character)
 end
 
 player.CharacterAdded:Connect(OnCharacterAdded)
