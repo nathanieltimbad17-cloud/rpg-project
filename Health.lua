afafbf99ae8d9c85e94c8d95a907ffc2cd4113fd
nathanieltimbad17-cloud <nathanieltimbad17@gmail.com>
2026-04-11 00:28:32 +0800

    Update Health.lua

--- Health.lua
+++ Health.lua
@@ -7,11 +7,19 @@ local Stats = require(ReplicatedStorage.Modules.Stats)
 local character : Model = script.Parent
 local humanoid : Humanoid = character.Humanoid
 
-local player = Players:GetPlayerFromCharacter(character)
+local player : Player = Players:GetPlayerFromCharacter(character)
 
-local stats = player:WaitForChild("Stats")
-local strength = stats:WaitForChild("Strength")
-local healthRegeneration = stats:WaitForChild("HealthRegeneration")
+local stats : Folder = player:WaitForChild("Stats")
+local strength : IntValue= stats:WaitForChild("Strength")
+local healthRegeneration : NumberValue = stats:WaitForChild("HealthRegeneration")
+
+local function OnHeartbeat()
+    if humanoid.Health < humanoid.MaxHealth then
+        humanoid.Health = math.min(humanoid.Health + healthRegeneration.Value, humanoid.MaxHealth)
+    end
+end
+
+RunService.Heartbeat:Connect(OnHeartbeat)
 
 local function UpdateHealth()
     local healthPercent = Stats.GetHealthPercent(humanoid)
@@ -22,11 +30,3 @@ end
 UpdateHealth()
 
 strength.Changed:Connect(UpdateHealth)
-
-while true do
-    if humanoid.Health < humanoid.MaxHealth then
-        humanoid.Health = math.min(humanoid.Health + healthRegeneration.Value, humanoid.MaxHealth)
-    end
-    
-    task.wait(Stats.RegenerationInterval.Value)
-end
